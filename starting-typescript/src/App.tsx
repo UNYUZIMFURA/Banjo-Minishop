3c683fc32dcf3035123e66cc9387f566af452b72
JoyKevin1 <joykevinrobin1@gmail.com>
2022-09-24 21:14:20 +0200

    Final push

--- starting-typescript/src/App.tsx
+++ starting-typescript/src/App.tsx
@@ -1,40 +1,6 @@
-let name: any;
-let age: number | string;
-let isStudent: boolean;
-let hobbies: string[];
-let role: [number, string];
+import React from "react";
 
-let printName: (name: string) => never;
-
-// function printName(name:string) {
-// console.log(name)
-// }
-
-// printName('You')
-// type Person = {
-//   name: string;
-//   age?: number;
-// };
-
-// let person: Person = {
-//   name: 'Kevin',
-//    age: 22
-// }
-
-// let lostOfPeople: Person[]
-
-interface Person {
-
-}
-
-type X = {
-  a: string
-  b: number;
-}
-
-let personName: unknown;
-
-function App() {
+const App: React.FC = () => {
   return <div className="App">Hello Typescript</div>;
 }
 
