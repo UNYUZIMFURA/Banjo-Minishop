4d8b0b604a30e952c7e360e6e4edb21430751ea3
JoyKevin1 <joykevinrobin1@gmail.com>
2022-09-26 21:43:46 +0200

    Lemme stop by here!

--- starting-typescript/src/components/SingleTodo.tsx
+++ starting-typescript/src/components/SingleTodo.tsx
@@ -1,6 +1,16 @@
 import React from 'react'
+import {RiDeleteBin4Fill} from "react-icons/ri"
+import {FiEdit} from "react-icons/fi"
+import {MdOutlineFileDownloadDone} from "react-icons/md"
+import { Todo } from './model'
 
-const SingleTodo = () => {
+type Props = {
+    todo: Todo,
+    todos: Todo[]
+    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const SingleTodo:React.FC<Props> = ({todo, todos, setTodos}) => {
   return (
     <div>SingleTodo</div>
   )
