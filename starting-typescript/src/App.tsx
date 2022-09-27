6644aaeaab55d1917fce17e0bdb3f56c789c1d14
JoyKevin1 <banjoflylive@gmail.com>
2022-09-27 22:41:41 +0200

    Another small change

--- starting-typescript/src/App.tsx
+++ starting-typescript/src/App.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 import Product from "./Components/Product";
 import NavBar from "./Components/NavBar";
-import Piano from "./Images/piano.jpg"
-import BMW from './Images/bmw.jpg'
-import Mercedez from './Images/mercedez.jpg'
-import Macbook from './Images/macbook.jpg'
+import Piano from "./Images/piano.jpg";
+import BMW from "./Images/bmw.jpg";
+import Mercedez from "./Images/mercedez.jpg";
+import Macbook from "./Images/macbook.jpg";
 
 const App: React.FC = () => {
   return (
     <div className="flex flex-col items-center h-screen w-screen border-[5px] border-[gray]">
       <NavBar />
       <div className="flex justify-around flex-wrap w-[90%] border-[1px] border-[blue] mt-[1rem]">
-        <Product img={Piano}/>
-        <Product img={BMW}/>
-        <Product img={Mercedez}/>
-        <Product img={Macbook}/>
-        <Product img={BMW}/>
-        <Product img={Mercedez}/>
+        <Product img={Piano} id="1"/>
+        <Product img={BMW} id="2"/>
+        <Product img={Mercedez} id="3"/>
+        <Product img={Macbook} id="4"/>
+        <Product img={BMW} id="5"/>
+        <Product img={Mercedez} id="6"/>
       </div>
     </div>
   );
--- starting-typescript/src/Components/NavBar.tsx
+++ starting-typescript/src/Components/NavBar.tsx
@@ -1,14 +1,18 @@
 import React from "react";
-import {BsCartPlusFill} from "react-icons/bs"
+import { BsCartPlusFill } from "react-icons/bs";
 
-const NavBar:React.FC = () => {
-  return <div className="relative flex justify-center items-center h-[10vh] w-full border-[1px] border-[blue]">
-    <h1 className="text-[2rem] font-medium">Typescript Cart</h1>
-    <span className="cursor-pointer absolute right-[5%]"><BsCartPlusFill size={30}/>
-    <div className="flex justify-center items-center absolute right-[-0.3rem] bottom-[-0.5rem] rounded-full bg-[red] h-[1.2rem] w-[1.2rem] text-[0.8rem] text-[white]">1</div>
-    </span>
-
-  </div>;
+const NavBar: React.FC = () => {
+  return (
+    <div className="relative flex justify-center items-center h-[10vh] w-full border-[1px] border-[blue]">
+      <h1 className="text-[2rem] font-medium">Typescript Cart</h1>
+      <span className="cursor-pointer absolute right-[5%]">
+        <BsCartPlusFill size={30} />
+        <div className="flex justify-center items-center absolute right-[-0.3rem] bottom-[-0.5rem] rounded-full bg-[red] h-[1.2rem] w-[1.2rem] text-[0.8rem] text-[white]">
+          1
+        </div>
+      </span>
+    </div>
+  );
 };
 
 export default NavBar;
