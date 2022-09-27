32f9d7cd16c1f170099909dc0cf380917f7f5692
JoyKevin1 <joykevinrobin1@gmail.com>
2022-09-27 21:52:47 +0200

    Added some images in product components

--- starting-typescript/src/App.tsx
+++ starting-typescript/src/App.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+import Product from "./Components/Product";
 import NavBar from "./Components/NavBar";
+import Piano from "./Images/piano.jpg"
+import BMW from './Images/bmw.jpg'
+import Mercedez from './Images/mercedez.jpg'
+import Macbook from './Images/macbook.jpg'
 
 const App: React.FC = () => {
   return (
-    <div className="flex flex-col h-screen w-screen border-[5px] border-[gray]">
+    <div className="flex flex-col items-center h-screen w-screen border-[5px] border-[gray]">
       <NavBar />
+      <div className="flex justify-around flex-wrap h-[80vh] w-[90%] border-[1px] border-[blue]">
+        <Product img={Piano}/>
+        <Product img={BMW}/>
+        <Product img={Mercedez}/>
+      </div>
     </div>
   );
 };
--- starting-typescript/src/Components/Product.tsx
+++ starting-typescript/src/Components/Product.tsx
@@ -1,5 +1,16 @@
-const Product = () => {
+interface Props {
+img: string
+}
 
+const Product:React.FC<Props> = ({img}) => {
+return (
+    <div className="h-[50vh] w-[30%] border-[1px] border-[yellow] flex flex-col">
+<div className="h-1/2 w-full">
+    <img src={img} className="object-cover h-full w-full" alt=""/>
+</div>
+<div className="h-1/2 w-full"></div>
+    </div>
+)
 }
 
 export default Product
