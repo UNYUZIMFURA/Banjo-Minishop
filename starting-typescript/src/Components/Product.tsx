e7117c4e563900b25e7dc0d806b13933bebafa7b
JoyKevin1 <banjoflylive@gmail.com>
2022-09-27 22:07:14 +0200

    ...

--- starting-typescript/src/Components/Product.tsx
+++ starting-typescript/src/Components/Product.tsx
@@ -6,14 +6,14 @@ const Product:React.FC<Props> = ({img}) => {
 return (
     <div className="h-[50vh] w-[30%] border-[1px] border-[yellow] flex flex-col">
 <div className="h-1/2 w-full">
-    <img src={img} className="object-cover h-full w-full" alt=""/>
+    <img src={img} className="object-cover h-full w-full cursor-pointer" alt=""/>
 </div>
 <div className="flex flex-col justify-around h-1/2 w-full">
     <div className="flex justify-around">
         <h1>Book</h1>
         <h2>$10</h2>
     </div>
-    <button>Add To Cart</button>
+    <button className="bg-[blue] text-[#fff] rounded-[3px] h-[3rem] cursor-pointer outline-none">Add To Cart</button>
 </div>
     </div>
 )
