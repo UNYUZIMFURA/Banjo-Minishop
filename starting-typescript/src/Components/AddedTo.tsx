1d0238f3667b4a99fa7b97edc35f8ebd8df041a6
JoyKevin1 <banjoflylive@gmail.com>
2022-09-28 08:59:20 +0200

    Constant pushing

--- starting-typescript/src/Components/AddedTo.tsx
+++ starting-typescript/src/Components/AddedTo.tsx
@@ -1,7 +1,23 @@
-const AddedTo = () => {
-    return (
-<div className="w-full h-[25vh] mt-[1rem] border-[1px] border-[green]"></div>
-    )
-}
+import {IoCloseCircleOutline} from "react-icons/io5"
 
-export default AddedTo
+const AddedTo = () => {
+  return (
+    <div className="flex justify-between w-full h-[25vh] mt-[1rem] bg-[#E8E8E8]">
+      <div className="bg-[green] h-full w-[55%] flex flex-col justify-evenly">
+        <div className="h-[70%] bg-[red]">{/* <img src={} /> */}</div>
+        <div className="flex w-full justify-around">
+          <h1>Book</h1>
+          <h1>Book</h1>
+        </div>
+      </div>
+      <div className="h-full w-[30%] bg-[cyan] flex flex-col justify-center ">
+        <h1>Quantity: 2</h1>
+        <h2>Total $30 </h2>
+      </div>
+      <div className="h-full w-[15%] bg-[cyan] flex flex-col">
+      </div>
+    </div>
+  );
+};
+
+export default AddedTo;
--- starting-typescript/src/Components/SideCart.tsx
+++ starting-typescript/src/Components/SideCart.tsx
@@ -4,7 +4,7 @@ import AddedTo from "./AddedTo";
 const SideCart = () => {
     return (
         <div className="flex-col justify-between items-center absolute right-0 overflow-y-scroll h-screen w-[25%] border-[1px] border-[red]">
-        <div className="flex justify-around w-full">
+        <div className="flex items-center h-[10vh] flex justify-around w-full">
           <h1 className="text-[1.3rem]">Inside Cart</h1>
           <VscChromeClose size={28}/>
         </div>
