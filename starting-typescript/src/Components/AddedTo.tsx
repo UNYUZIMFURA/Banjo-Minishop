f350e231bf2ff384ac413000ec7edaff86ed83c3
JoyKevin1 <banjoflylive@gmail.com>
2022-09-28 15:35:14 +0200

    Nothing special

--- starting-typescript/src/Components/AddedTo.tsx
+++ starting-typescript/src/Components/AddedTo.tsx
@@ -1,20 +1,25 @@
 import {IoCloseCircleOutline} from "react-icons/io5"
 
+interface Props {
+
+}
+
 const AddedTo = () => {
   return (
-    <div className="flex justify-between w-full h-[25vh] mt-[1rem] bg-[#E8E8E8]">
-      <div className="bg-[green] h-full w-[55%] flex flex-col justify-evenly">
-        <div className="h-[70%] bg-[red]">{/* <img src={} /> */}</div>
+    <div className="flex justify-between items-center w-full h-[25vh] mt-[1rem] bg-[#E8E8E8]">
+      <div className="h-full w-[55%] flex flex-col justify-evenly">
+        <div className="h-[70%]">{/* <img src={} /> */}</div>
         <div className="flex w-full justify-around">
           <h1>Book</h1>
           <h1>Book</h1>
         </div>
       </div>
-      <div className="h-full w-[30%] bg-[cyan] flex flex-col justify-center ">
-        <h1>Quantity: 2</h1>
-        <h2>Total $30 </h2>
+      <div className="h-1/2 w-[30%] flex flex-col justify-around">
+        <h1 className="">Quantity: 2</h1>
+        <h2 className="">Total $30 </h2>
       </div>
-      <div className="h-full w-[15%] bg-[cyan] flex flex-col">
+      <div className="h-full w-[10%] flex flex-col items-center justify-center">
+     <IoCloseCircleOutline size={35} />
       </div>
     </div>
   );
--- starting-typescript/src/Components/SideCart.tsx
+++ starting-typescript/src/Components/SideCart.tsx
@@ -3,7 +3,7 @@ import AddedTo from "./AddedTo";
 
 const SideCart = () => {
     return (
-        <div className="flex-col justify-between items-center absolute right-0 overflow-y-scroll h-screen w-[25%] border-[1px] border-[red]">
+        <div className="flex-col justify-between items-center absolute right-0 overflow-y-scroll h-screen w-[31%] border-[1px] border-[red]">
         <div className="flex items-center h-[10vh] flex justify-around w-full">
           <h1 className="text-[1.3rem]">Inside Cart</h1>
           <VscChromeClose size={28}/>
