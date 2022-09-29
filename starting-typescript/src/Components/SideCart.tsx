43f606307034ec312b6e122b1aa63e7b713e8d90
JoyKevin1 <banjoflylive@gmail.com>
2022-09-29 11:48:03 +0200

    Still working on the cart

--- starting-typescript/src/Components/SideCart.tsx
+++ starting-typescript/src/Components/SideCart.tsx
@@ -5,8 +5,8 @@ import StateContext from "../helpers/useContext";
 
 const SideCart: React.FC = (): any => {
   const { openVal, setOpenVal } = useContext(StateContext) as any;
-  console.log(openVal)
-  if (openVal) {
+  console.log(openVal);
+  return openVal ? (
     <div className="flex-col justify-between items-center absolute right-0 overflow-y-scroll h-screen w-[31%] border-[1px] border-[red]">
       <div className="flex items-center h-[10vh] flex justify-around w-full">
         <h1 className="text-[1.3rem]">Inside Cart</h1>
@@ -22,10 +22,10 @@ const SideCart: React.FC = (): any => {
       <AddedTo />
       <AddedTo />
       <AddedTo />
-    </div>;
-  } else {
-    console.log('No')
-  }
+    </div>
+  ) : (
+    <></>
+  );
 };
 
 export default SideCart;
