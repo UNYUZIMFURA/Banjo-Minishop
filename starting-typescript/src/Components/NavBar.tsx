9ccb470c524ed3d64f58a958073eca6b2310c674
JoyKevin1 <banjoflylive@gmail.com>
2022-09-29 11:11:15 +0200

    Working on hide/display cart

--- starting-typescript/src/Components/NavBar.tsx
+++ starting-typescript/src/Components/NavBar.tsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BsCartPlusFill } from "react-icons/bs";
+import StateContext from "../helpers/useContext";
 
-const NavBar = () => {
+const NavBar: React.FC = () => {
+  const { openVal, setOpenVal } = useContext(StateContext) as any;
+
   return (
     <div className="relative flex justify-center items-center h-[10vh] w-full">
       <h1 className="text-[2rem] font-medium">Typescript Cart</h1>
       <span className="cursor-pointer absolute right-[8%]">
-        <BsCartPlusFill size={30} />
+        <div onClick={() => {setOpenVal(true)}}>
+          <BsCartPlusFill size={30} />
+        </div>
         <div className="flex justify-center items-center absolute right-[-0.3rem] bottom-[-0.5rem] rounded-full bg-[red] h-[1.2rem] w-[1.2rem] text-[0.8rem] text-[white]">
           1
         </div>
--- starting-typescript/src/Components/SideCart.tsx
+++ starting-typescript/src/Components/SideCart.tsx
@@ -3,13 +3,14 @@ import { VscChromeClose } from "react-icons/vsc";
 import AddedTo from "./AddedTo";
 import StateContext from "../helpers/useContext";
 
-const SideCart: React.FC = ():any => {
+const SideCart: React.FC = (): any => {
   const { openVal, setOpenVal } = useContext(StateContext) as any;
-  if(openVal) {
+  console.log(openVal)
+  if (openVal) {
     <div className="flex-col justify-between items-center absolute right-0 overflow-y-scroll h-screen w-[31%] border-[1px] border-[red]">
       <div className="flex items-center h-[10vh] flex justify-around w-full">
         <h1 className="text-[1.3rem]">Inside Cart</h1>
-        <div onClick={() => setOpenVal((prevVal: boolean) => !prevVal)}>
+        <div>
           <VscChromeClose size={28} />
         </div>
       </div>
@@ -21,9 +22,9 @@ const SideCart: React.FC = ():any => {
       <AddedTo />
       <AddedTo />
       <AddedTo />
-    </div>
+    </div>;
   } else {
-    return null
+    console.log('No')
   }
 };
 
