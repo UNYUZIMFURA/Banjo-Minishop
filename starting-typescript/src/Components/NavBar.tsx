06fc77e1b68e8dbaabc950d9d77598fc13f5656d
JoyKevin1 <banjoflylive@gmail.com>
2022-10-01 21:29:39 +0200

    Starting to work on calculating total quantity

--- starting-typescript/src/Components/NavBar.tsx
+++ starting-typescript/src/Components/NavBar.tsx
@@ -1,9 +1,10 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { BsCartPlusFill } from "react-icons/bs";
 import StateContext from "../helpers/useContext";
 
 const NavBar: React.FC = () => {
   const { setOpenVal } = useContext(StateContext) as any;
+  const { productsArr } = useContext(StateContext) as any;
 
   return (
     <div className="relative flex justify-center items-center h-[10vh] w-full">
@@ -17,7 +18,7 @@ const NavBar: React.FC = () => {
           <BsCartPlusFill size={30} />
         </div>
         <div className="flex justify-center items-center absolute right-[-0.3rem] bottom-[-0.5rem] rounded-full bg-[red] h-[1.2rem] w-[1.2rem] text-[0.8rem] text-[white]">
-          1
+          {productsArr.length}
         </div>
       </span>
     </div>
